ce20d8282d7595b3105d9c2047102d63ab419ba1
jue ma <ykhinama@gmail.com>
2024-07-29 14:29:46 -0700

    sort by page number

--- sort_page.c
+++ sort_page.c
@@ -6,22 +6,75 @@
 
 int main(int argc, char **argv) {
     printf("hello\n");
+    int dryrun = 0;
+    char *filename = NULL;
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-n") == 0) {
+                dryrun = 1;
+            } else {
+                filename = argv[i];
+            }
+        }
+    }
+    if (!filename) {
+        printf("Usage: %s [-n] <FILENAME>", argv[0]);
+        exit(1);
+    }
     unsigned char *buffer = (unsigned char*)malloc(PAGE_SIZE); // 16k page
-    FILE *f = fopen("00000594.pages", "r");
+    FILE *f = fopen(filename, "r");
+    FILE *output = NULL;
+    unsigned char *zero_page = NULL;
+    size_t cur_file_size = 0;
+    if (!dryrun) {
+        char *filename_sorted = malloc(strlen(filename) + 7 + 1);
+        strcpy(filename_sorted, filename);
+        strcat(filename_sorted, ".sorted");
+        output = fopen(filename_sorted, "w");
+        zero_page = (unsigned char*)calloc(1, PAGE_SIZE);
+    }
     if (f) {
         while (fread(buffer, PAGE_SIZE, 1, f) == 1) {
-            // char* buffer2 = (char*)malloc(4);
-            // buffer2[0] = buffer[4];
-            // memcpy(buffer2, &buffer[4], 4);
-            uint32_t offset = (uint32_t)buffer[4] << 24 | (uint32_t)buffer[5] << 16 | (uint32_t)buffer[6] << 8 | buffer[7];
+            uint32_t page_number = (uint32_t)buffer[4] << 24 | (uint32_t)buffer[5] << 16 | (uint32_t)buffer[6] << 8 | buffer[7];
             uint32_t space_id = (uint32_t)buffer[34] << 24 | (uint32_t)buffer[35] << 16 | (uint32_t)buffer[36] << 8 | buffer[37];
-            printf("offset: %u\n", offset);
-            printf("space_id: %u\n", space_id);
-            // sort by page number
+            if (!dryrun) {
+                printf("offset: %u\n", page_number);
+                printf("space_id: %u\n", space_id);
+                // sort by page number
+                if (output) {
+                    // write pagenumber - filesize zero pages
+                    if (page_number > cur_file_size) {
+                        if (fseek(output, 0, SEEK_END) == 0) {
+                            while (page_number > cur_file_size) {
+                                if (fwrite(zero_page, PAGE_SIZE, 1, output) != 1) {
+                                    exit(1);
+                                }
+                                cur_file_size++;
+                            }
+                        } else {
+                            printf("failed to seek\n");
+                            exit(1);
+                        }
+                    }
+                    if (fseek(output, page_number * PAGE_SIZE, SEEK_SET) == 0) {
+                        if (fwrite(buffer, PAGE_SIZE, 1, output) != 1) {
+                            exit(1);
+                        }
+                        if (page_number == cur_file_size) {
+                            cur_file_size++;
+                        }
+                    } else {
+                        printf("failed to seek\n");
+                        exit(1);
+                    }
+                }
+            }
+            
         }
     } else {
         printf("can't open the file.\n");
         return 1;
     }
+    fclose(output);
     return 0;
 }
